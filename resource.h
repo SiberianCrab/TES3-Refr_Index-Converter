aad0d32b59946cc5d08b276d4e5713d25a052ef2
SiberianCrab <46442176+SiberianCrab@users.noreply.github.com>
2024-11-01 14:43:11 +0300

    Icon
    Icon
    Clean Up

--- resource.h
+++ resource.h
@@ -1,13 +1,14 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by Refr_Index Mod Converter.rc
+// Used by TES3 Refr_Index Converter.rc
 //
+#define IDI_ICON1                       114
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        115
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
